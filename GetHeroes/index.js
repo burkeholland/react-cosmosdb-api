109340a7a875c4b28d140003bed865830fe92ac3
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-10-21 15:20:31 -0500

    revert GetHeroes

--- GetHeroes/index.js
+++ GetHeroes/index.js
@@ -1,62 +1,34 @@
 // const Hero = require('../hero-model');
 // const mongodb = require('../mongodb');
 
-// const MongoClient = require('mongodb');
+const MongoClient = require('mongodb');
 
 module.exports = function(context, req) {
-  //   try {
-  //     MongoClient.connect(process.env.CosmosDBConnectionString, (err, db) => {
-  //       let collection = db.collection('heros');
+  try {
+    mongodb.connect();
+    const docquery = Hero.find({}).read();
+    docquery
+      .exec()
+      .then(heroes => {
+        context.res = {
+          body: heroes
+        };
 
-  //       collection.find({}).toArray((err, docs) => {
-  //         context.res = {
-  //           body: 'ok'
-  //         };
+        context.done();
+      })
+      .catch(err => {
+        context.res = {
+          status: 500,
+          body: err.message
+        };
 
-  //         context.done();
+        context.done();
+      });
+  } catch (error) {
+    context.res = {
+      body: error
+    };
 
-  //         db.close();
-  //       });
-  //     });
-  //   } catch (err) {
-  //     context.res = {
-  //       status: 500,
-  //       body: err
-  //     };
-
-  //     context.done();
-  //   }
-
-  context.res = {
-    body: 'hi'
-  };
-
-  context.done();
+    context.done();
+  }
 };
-//     mongodb.connect();
-//     const docquery = Hero.find({}).read();
-//     docquery
-//       .exec()
-//       .then(heroes => {
-//         context.res = {
-//           body: heroes
-//         };
-
-//         context.done();
-//       })
-//       .catch(err => {
-//         context.res = {
-//           status: 500,
-//           body: err.message
-//         };
-
-//         context.done();
-//       });
-//   } catch (error) {
-//     context.res = {
-//       body: error
-//     };
-
-//     context.done();
-//   }
-// };
