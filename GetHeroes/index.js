4783f8269cdad717857097d0d55a809c68429ad8
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-10-30 08:54:12 -0500

    Fix GetHeroes

--- GetHeroes/index.js
+++ GetHeroes/index.js
@@ -1,49 +1,20 @@
-// var MongoClient = require('mongodb').MongoClient;
-
-// module.exports = function(context, req) {
-//   MongoClient.connect(process.env.CosmosDBConnectionString, function(err, db) {
-//     if (err) throw err;
-
-//     db
-//       .collection('heros')
-//       .find({})
-//       .toArray((err, result) => {
-//         if (err) throw err;
-
-//         result.forEach(item => {
-//           delete item._id;
-//         });
-
-//         context.res = {
-//           body: JSON.parse(JSON.stringify(result))
-//         };
-//         db.close();
-//         context.done();
-//       });
-//   });
-// };
-
 var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function(context, req) {
   MongoClient.connect(process.env.CosmosDBConnectionString, function(err, db) {
     if (err) throw err;
 
-    let hero = ({ id, name, saying } = req.body);
-
-    db.heros.update(
-      { id: 1 },
-      { id: 1, name: 'ok', saying: 'you got it man!' },
-      (err, heros) => {
-        context.log(heros);
-
+    db
+      .collection('heros')
+      .find({})
+      .toArray((err, result) => {
         if (err) throw err;
+
         context.res = {
-          body: hero
+          body: JSON.parse(JSON.stringify(result))
         };
         db.close();
         context.done();
-      }
-    );
+      });
   });
 };
